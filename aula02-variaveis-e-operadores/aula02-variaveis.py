d72d37e53dd40465e8b7660594bc6a3ad83d6d1e
odaniell007 <danithefirst15@gmail.com>
2026-03-20 20:50:18 -0300

    aula 2 variáveis e operadores

---
+++ aula02-variaveis-e-operadores/aula02-variaveis.py
@@ -0,0 +1,31 @@
+print ('Olá Mundo')
+
+print (7+4)
+print ('7+4')
+print ('7' + '4') # CONTENAÇÃO DE STRINGS
+
+# COMENTÁRIOS EM PYTHON DE 1 LINHA
+'''
+Comentários de 
+múltiplas
+linhas
+'''
+
+# Variáveis
+nome = 'Leandro' # string
+idade = 26  # int
+peso = 70.2 # float
+
+print (nome, idade, peso)
+print(f'Oiiii, {nome}!!!')
+
+nome = input('Digite o seu nome: ')
+print(nome)
+idade = int(input('Digite a sua idade: '))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
+
+
---
+++ aula02-variaveis-e-operadores/desafios/desafios.py
@@ -0,0 +1,8 @@
+'''nome = input('Digite seu nome: ')
+print(f'Seja bem vindo {nome}!')
+'''
+
+num1 = int(input(f'Digite o dia de seu nascimento: '))
+num2 = int(input(f'Digite o mês de seu nascimento: '))
+num3 = int(input(f'Digite o ano de seu nascimento: '))
+print(f'{num1}/{num2}/{num3}')
